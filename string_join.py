fa4a2348372e2b3928d2caeab36fbc6f3131237d
siva <siva.m@corestack.io>
2020-12-05 17:48:46 +0530

    Day 1 completion

---
+++ string_join.py
@@ -0,0 +1,21 @@
+# user_string = input("enter a word: ")
+
+# data = ''.join(user_string.split(','))
+# print(data)
+
+if 
+elif 
+else
+
+user_input > 18:
+	should run if 
+
+user_input < 18:
+	should run elif
+
+user_input thrid case: 65 and above not eligible
+
+and or not 
+
+& ! !=
+
